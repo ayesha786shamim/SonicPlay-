6e1342157890ce777ac8a4bc7d58e7853614832b
ahmed <ahmedtasadduq6123@gmail.com>
2024-12-24 02:53:09 +0500

    Added Application Class to create Channel For NotifService

---
+++ app/src/main/java/com/example/musicapplication/Controller/ApplicationClass.java
@@ -0,0 +1,30 @@
+package com.example.musicapplication.Controller;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class ApplicationClass extends Application {
+    public static final String CHANNEL_ID = "music_service_channel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Music Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(serviceChannel);
+            }
+        }
+    }
+}
